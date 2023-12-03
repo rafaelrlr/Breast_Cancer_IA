91ffd1b8d3932a5b9829ce762595f7219bc38f5f
wesley369 <wesley201075@gmail.com>
2023-12-03 20:32:07 -0300

    Ajustes no código

--- breast_ai/diagnostico/static/integracao.js
+++ breast_ai/diagnostico/static/integracao.js
@@ -1,7 +1,6 @@
 var formData = {};
 
 document.getElementById('btnPrevisao').addEventListener('click', function () {
-    // Obter os dados do formulário
     formData = {
         campo1: parseFloat(document.getElementById('campo1').value),
         campo2: parseFloat(document.getElementById('campo2').value),
@@ -10,7 +9,6 @@ document.getElementById('btnPrevisao').addEventListener('click', function () {
         campo5: parseFloat(document.getElementById('campo5').value)
     };
 
-    // Enviar dados para o servidor usando fetch
     fetch('/realizar_predicao/', {
         method: 'POST',
         headers: {
@@ -18,20 +16,20 @@ document.getElementById('btnPrevisao').addEventListener('click', function () {
         },
         body: JSON.stringify(formData),
     })
+    
     .then(response => {
         if (!response.ok) {
             throw new Error('Erro na solicitação.');
         }
         return response.json();
     })
+
      .then(data => {
-         // Atualizar o campo de resultado com a previsão
          document.getElementById('resultado').value = data.predicao; 
      })
 
      .catch(error => {
         console.error('Erro:', error);
-        // Trate o erro conforme necessário
     });
 });
  
--- breast_ai/diagnostico/views.py
+++ breast_ai/diagnostico/views.py
@@ -4,7 +4,7 @@ import joblib
 import json
 from django.views.decorators.csrf import csrf_exempt
 
-# Carregar o modelo .pkl
+
 modelo = joblib.load('./diagnostico/modeloTreinamento.pkl')
 
 def index(request):
@@ -13,17 +13,17 @@ def index(request):
 @csrf_exempt
 def realizar_predicao(request):
      try:
-         # Obter os dados do formulário
+         
          dados = json.loads(request.body)
 
-         # Realizar a previsão usando o modelo
+         
          predicao = modelo.predict([[dados['campo1'], dados['campo2'], dados['campo3'], dados['campo4'], dados['campo5']]])
 
-         # Converta o resultado do modelo para string ('Maligno' ou 'Benigno')
+         
          predicao_str = 'Benigno' if predicao[0] == 1 else 'Maligno'
 
 
-         # A resposta é enviada de volta ao frontend
+         
          return JsonResponse({'predicao': predicao_str})
      except Exception as e:
          return JsonResponse({'error': str(e)})
